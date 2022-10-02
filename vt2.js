050d5e7941a21a55246dbbc9d726e8f4364ccd07
Anniina Aarnio <aarnipex@student.jyu.fi>
2022-10-02 10:00:48 +0300

    datasta mapit: sarjat, rastit, joukkueet

--- vt2.js
+++ vt2.js
@@ -17,33 +17,110 @@ reset = false;
 // älä kutsu tätä itse!
 function start(data) {
 
-        // luodaan taulukot, joissa id-avaimella viitteet datan nodeihin
-        let sarjat = luoTaulukkoIdeista(data, "sarja");
-        let rastit = luoTaulukkoIdeista(data, "rasti");
+        // luodaan mapit, joissa id -> node -parit
+        let sarjat = luoMapIdeista(data, "sarja");
+        let rastit = luoMapIdeista(data, "rasti");
+        // luodaan map, jossa joukkueet nimi -> node -pareina
+        let joukkueet = luoJoukkueetMap(data);
+        console.log(sarjat);
+        console.log(rastit);
+        console.log(joukkueet);
         
+        // luodaan taulukon sisällöt, jotka järjestyksessä ensin sarjan sitten joukkueennimen mukaan
+        luoTaulukonRivit(sarjat, joukkueet);
+        // luodaan lista kaikkien rastien koodeista aakkosjärjestyksessä
+
+        // luodaan lomake, jolla voi lisätä XML-rakenteeseen uuden rastin
+        // documents.forms-rajapinnan kautta
+
         // dataa voi tutkia myös osoitteesta: https://appro.mit.jyu.fi/cgi-bin/tiea2120/randomize.cgi
         // huom. datan sisältö muuttuu hieman jokaisella latauskerralla
-	console.log(data.documentElement);
-	console.log(data.documentElement.getElementsByTagName("joukkue"));
         // savedata tallentaa datan selaimen LocalStorageen. Tee tämä aina, kun
         // ohjelmasi on muuttanut dataa. Seuraavalla sivun latauskerralla
         // saat käyttöösi muuttamasi datan
 	savedata(data);
 }
 
+// ----- OMAT FUNKTIOT -----
+
 /**
  * Luodaan objekti, johon lisätään kyseisellä sanalla etsittyjä
  * nodeja datasta siten, että noden id on objektissa avain ja arvo on viite kyseiseen nodeen
  * @param {XMLDocument} data
- * @param {String} sana etsittävän sanan perusteella
+ * @param {String} hakusana etsittävän sanan perusteella
  * @return {Object} objekti, jossa kyseisellä sanalla tägätyt nodet 
  */
-function luoTaulukkoIdeista(data, sana) {
-        let jutut = {};
-        for (let lapsi of data.documentElement.getElementsByTagName(sana)) {
-                jutut[lapsi.id] = lapsi;
-        }
-        console.log(jutut);
-        return jutut;
+function luoMapIdeista(data, hakusana) {
+        let jutut = new Map();
 
+        // luo id-sisältöisen avaimen jokaisesta lapsesta (eli sarjasta / rastista)
+        for (let lapsi of data.documentElement.getElementsByTagName(hakusana)) {
+                jutut.set(lapsi.id, lapsi);
+        }
+        return jutut;
+}
+
+/**
+ * Luodaan objekti, johon lisätään joukkuenodet datasta siten, että
+ * joukkueen nimi on objektissa avain ja arvo on viite kyseiseen nodeen
+ * @param {XMLDocument} data
+ * @return {Map} objekti, jossa joukkueet nimi -> node -pareina
+ */
+function luoJoukkueetMap(data) {
+        let joukkueet = new Map();
+        for (let joukkue of data.documentElement.getElementsByTagName("joukkue")) {
+                joukkueet.set(joukkue.lastElementChild.textContent.trim(), joukkue);
+        }
+        return joukkueet;
+}
+
+function luoTaulukonRivit(sarjat, joukkueet) {
+        // järjestä sarjat aakkosjärjestykseen
+        let sarjalista = [];
+        for (let sarja in sarjat) {
+                sarjalista.push(sarja);
+        }
+
+        // järjestä joukkueet aakkosjärjestykseen
+
+        // luo
+}
+
+
+
+// ----- OMAT APUFUNKTIOT -----
+
+/**
+ * Vertaa kahta annettua merkkijonoa:
+ * muuntaa kaikki pieniksi kirjaimiksi ja vertaa sen jälkeen
+ * @param {String} a 
+ * @param {String} b
+ * @return -1 jos a ensin, 1 jos b ensin, 0 jos samat 
+ */
+function vertaaKaikkiPienella(a, b) {
+        // muutetaan kaikki kirjaimet pieniksi + whitespacet pois alusta ja lopusta
+        let aa = a.toLowerCase().trim();
+        let bb = b.toLowerCase().trim();
+        
+        // vertaa toisiinsa
+        if (aa < bb) {
+                return -1;
+        } else if (bb < aa) {
+                return 1;
+        }
+        return 0;
+}
+
+/**
+ * Vertaa kahta annettua merkkijonoa:
+ * kirjaimet tulevat ennen numeroita, isoilla ja pienillä kirjaimilla ei väliä
+ * @param {String} a 
+ * @param {String} b 
+ * @returns -1 jos a ensin, 1 jos b ensin ja 0 jos samat
+ */
+function vertaaKirjaimetEnnenNumeroita(a, b) {
+        // onko kirjaimia alussa
+
+        // kaikki pieniksi kirjaimiksi + trim
+        return 0;
 }
